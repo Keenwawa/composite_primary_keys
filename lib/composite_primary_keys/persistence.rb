dac27dc97af190bed006a742cb0959d6af684242
Kerey Roper <kereyroper@users.noreply.github.com>
2016-07-21 12:34:52 -0700

    merge results from INSERT into key
    Sometimes it is useful for only a portion of the primary key to be generated by
    the database. Merge the results from the INSERT back into the ActiveRecord
    object.

--- lib/composite_primary_keys/persistence.rb
+++ lib/composite_primary_keys/persistence.rb
@@ -60,6 +60,21 @@ module ActiveRecord
           true
         end
       end
+
+      def create_record(attribute_names = @attributes.keys)
+        attributes_values = arel_attributes_with_values_for_create(attribute_names)
+
+        new_id = self.class.unscoped.insert attributes_values
+        self.id ||= new_id if self.class.primary_key
+
+        # CPK
+        if self.class.primary_key && self.id.is_a?(Array) && new_id.is_a?(Array)
+          self.id = self.id.map.with_index{|x,i| x or new_id[i]}
+        end
+
+        @new_record = false
+        id
+      end
     end
   end
 end
