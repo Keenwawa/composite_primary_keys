91321c56e78742768e85eacf93da590ad7f7dab5
Kerey Roper <kereyroper@users.noreply.github.com>
2016-11-11 17:24:36 -0800

    fix

--- lib/composite_primary_keys/persistence.rb
+++ lib/composite_primary_keys/persistence.rb
@@ -56,7 +56,7 @@ module ActiveRecord
 
     # Creates a record with values matching those of the instance attributes
     # and returns its id.
-    def _create_record(attribute_names = @attributes.keys)
+    def _create_record(attribute_names = self.attribute_names)
       attributes_values = arel_attributes_with_values_for_create(attribute_names)
 
       new_id = self.class.unscoped.insert attributes_values
